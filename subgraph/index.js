de7ef1d51be6fd706118ed3d231c9d3e7106c3f8
neerajsu <neeraj.subramaniyan@gmail.com>
2025-12-02 16:07:05 +0530

    First commit

---
+++ subgraph/index.js
@@ -0,0 +1,50 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "fs";
+
+const typeDefs = readFileSync("./schema.graphql", "utf8");
+
+// --- Mock Data ---
+const authors = [
+  { id: "1", name: "George Orwell" },
+  { id: "2", name: "J.K. Rowling" },
+];
+
+const books = [
+  { id: "101", title: "1984", authorId: "1" },
+  { id: "102", title: "Animal Farm", authorId: "1" },
+  { id: "201", title: "Harry Potter and the Sorcerer's Stone", authorId: "2" },
+];
+
+// --- Resolvers ---
+const resolvers = {
+  Query: {
+    hello: () => "Hello from the subgraph!",
+    books: () => books,
+    bookById: (_, { id }) => books.find((b) => b.id === id),
+    authors: () => authors,
+    authorById: (_, { id }) => authors.find((a) => a.id === id),
+  },
+
+  Book: {
+    author: (book) => authors.find((a) => a.id === book.authorId),
+  },
+
+  Author: {
+    books: (author) => books.filter((b) => b.authorId === author.id),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Start Apollo Server
+// Note the top-level await!
+const { url } = await startStandaloneServer(server, {
+  context: async ({ req }) => ({ token: req.headers.token }),
+  listen: { port: 4001 },
+});
+
+console.log(`🚀 Subgraph running at ${url}`);
